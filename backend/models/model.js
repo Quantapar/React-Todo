c92008dfe3947329880b4f79cb8507dae06ca008
Manu Sharma <manusharma2462@gmail.com>
2025-12-01 09:37:33 +0530

    Finish backend implementation

--- backend/models/model.js
+++ backend/models/model.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const todoSchema = mongoose.Schema({
   title: String,
   description: String,
-  completed: Boolean,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
