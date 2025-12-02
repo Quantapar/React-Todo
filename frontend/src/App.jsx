60d62753e99ef41ba1a7cadedaa8a12881837774
Manu Sharma <manusharma2462@gmail.com>
2025-12-02 10:16:59 +0530

    added delete and fetch functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3030/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json);
+    });
+  }, []);
+  async function markTodoComplete(id) {
+    await fetch("http://localhost:3030/complete", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
+
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo._id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  }
+  async function deleteTodo(id) {
+    await fetch(`http://localhost:3030/todo/${id}`, {
+      method: "DELETE",
+    });
+
+    setTodos((prev) => prev.filter((todo) => todo._id !== id));
+  }
+
   return (
     <div>
       <CreateTodo />
       <Todos
-        todos={[
-          { title: "Gym", description: "Leg day", completed: false },
-          { title: "Study", description: "React", completed: true },
-        ]}
-      ></Todos>
+        todos={todos}
+        onComplete={markTodoComplete}
+        onDelete={deleteTodo}
+      />
     </div>
   );
 }
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
 import "./CreateTodo.css";
 
 export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setdescription] = useState("");
+
   return (
     <div className="create-todo-container">
-      <input type="text" placeholder="Title" className="todo-input" />
-      <input type="text" placeholder="Description" className="todo-input" />
-      <button className="add-todo-button">Add a todo</button>
+      <input
+        type="text"
+        placeholder="Title"
+        className="todo-input"
+        onChange={function (e) {
+          setTitle(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        className="todo-input"
+        onChange={function (e) {
+          setdescription(e.target.value);
+        }}
+      />
+      <button
+        className="add-todo-button"
+        onClick={() => {
+          fetch("http://localhost:3030/todo", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json", // imp for zod validation
+            },
+            body: JSON.stringify({
+              title: title,
+              description: description,
+              completed: false,
+            }),
+          }).then(async function (res) {
+            const json = await res.json();
+            // alert("Todo Added");
+            window.location.reload();
+          });
+        }}
+      >
+        Add a todo
+      </button>
     </div>
   );
 }
